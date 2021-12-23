8af4f7aa75fe5d548ef4984311e94178dc56fd14
jiwon <jidole041214@naver.com>
2021-12-23 21:06:28 +0900

    add google addmob

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,21 +1,36 @@
 import { StyleSheet, View } from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
+import {
+  AdMobBanner,
+  AdMobInterstitial,
+  PublisherBanner,
+  AdMobRewarded,
+  setTestDeviceIDAsync,
+} from "expo-ads-admob";
 
 export default function Layout({ children }) {
   return (
     <View style={styles.wrapper}>
       <View style={styles.container}>{children}</View>
+      <AdMobBanner
+        bannerSize="banner"
+        adUnitID="ca-app-pub-2814958166637393~9752476790" // Test ID, Replace with your-admob-unit-id
+        servePersonalizedAds // true or false
+        onDidFailToReceiveAdWithError={(err) => {
+          console.log(err);
+        }}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   wrapper: {
-    flexDirection: "row",
-    justifyContent: "center",
+    flexDirection: "column",
+    alignItems: "center",
   },
   container: {
-    paddingVertical: 40,
+    paddingTop: 40,
+    paddingBottom: 30,
     paddingHorizontal: 30,
     width: 350,
   },
